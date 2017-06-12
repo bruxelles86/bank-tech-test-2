511bd55d72057490e9cf37d94c4666efbae293dd
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 19:03:14 +0100

    Adds 8 pending specs to account_spec.rb

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,21 @@
 require 'account'
 
 describe Account do
+  let(:account) { described_class.new }
 
+  it 'has a balance'
+
+  it 'has a record of transactions'
+
+  it 'initiates deposits'
+
+  it 'initiates the storage of transaction data after a deposit'
+
+  it 'initiates the storage of transaction data after a withdrawal'
+
+  it 'initiates withdrawals'
+
+  it 'records withdrawal details'
+
+  it 'initiates the printing of a statement to the console'
 end
