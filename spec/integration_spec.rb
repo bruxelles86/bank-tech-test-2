92456c0a7c366271b89af14555d8faf6212dcc27
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 23:11:57 +0100

    Adds failing integration test

---
+++ spec/integration_spec.rb
@@ -0,0 +1,17 @@
+require 'account'
+
+describe Account do
+  let(:account) { described_class.new(Balance, Log, Printer) }
+
+  it 'prints correct statement matching task acceptance criteria' do
+    account.deposit(1000, '10/01/2012')
+    account.deposit(2000, '13/01/2012')
+    account.withdraw(500, '14/01/2012')
+    expect { account.print_statement }.to output(
+      'Date || Credit || Debit || Balance'\
+    "\n14/01/2012 ||  || 500.00 || 2500.00\n"\
+    "\n13/01/2012 || 2000.00 ||  || 3000.00\n"\
+    "\n10/01/2012 || 1000.00 ||  || 1000.00\n"
+    ).to_stdout
+  end
+end
