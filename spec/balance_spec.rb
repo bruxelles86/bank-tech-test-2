c05ef5eaa37ed35047c9cf543a2a3690826c674e
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 21:47:42 +0100

    adds failing test for balances to have initial amounts of zero

--- spec/balance_spec.rb
+++ spec/balance_spec.rb
@@ -3,7 +3,9 @@ require 'balance'
 describe Balance do
   let(:balance) { described_class.new }
 
-  it 'initializes with an amount of zero'
+  it 'initializes with an amount of zero' do
+    expect(balance.amount).to eq(0)
+  end
 
   it 'increments by amounts deposited'
 
