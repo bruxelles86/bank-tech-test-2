d7a6b9e9525e73d3c3d3a54ec38ce3c13f24d14f
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 19:10:14 +0100

    Adds empty balance class and three pending specs

---
+++ spec/balance_spec.rb
@@ -0,0 +1,12 @@
+require 'balance'
+
+describe Balance do
+  let(:balance) { described_class.new }
+
+  it 'initializes with an amount of zero'
+
+  it 'increments by amounts deposited'
+
+  it 'reduces by amounts withdrawn'
+
+end
