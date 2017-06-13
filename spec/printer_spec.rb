024572428027d58982e83eaee573d42b0039af94
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-13 10:33:35 +0100

    Adds test to show statements correctly prints withdrawal details

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -12,4 +12,14 @@ describe Printer do
       "\n01/01/2000 || 10.00 ||  || 10.00\n"
       ).to_stdout
     end
+
+    it 'correctly prints a statement with withdrawals' do
+      log = instance_double("log", :transactions => [{ date: '01/01/2000',
+                                                       credit: 0, debit: 15.00,
+                                                       balance: 20.00 }] )
+        expect { printer.print(log.transactions) }.to output(
+        'Date || Credit || Debit || Balance'\
+        "\n01/01/2000 ||  || 15.00 || 20.00\n"
+        ).to_stdout
+      end
   end
