afe4b2d0824c60123bf916f40b0c7de57914f87b
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-13 11:20:40 +0100

    Alters printer spec to mock extracted sorter class

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,13 +1,17 @@
 require 'printer'
 
 describe Printer do
-  let(:printer) { described_class.new }
+  before(:each) do
+    sorter = double
+    allow(sorter).to receive_messages(:sort => nil, :new => sorter)
+    @printer = Printer.new(sorter)
+  end
 
   it 'correctly prints a statement with deposits' do
       log = instance_double("log", :transactions => [{ date: '01/01/2000',
                                                      credit: 10.00, debit: 0,
                                                      balance: 10.00 }] )
-      expect { printer.print(log.transactions) }.to output(
+      expect { @printer.print(log.transactions) }.to output(
       'Date || Credit || Debit || Balance'\
       "\n01/01/2000 || 10.00 ||  || 10.00\n"
       ).to_stdout
@@ -17,27 +21,31 @@ describe Printer do
       log = instance_double("log", :transactions => [{ date: '01/01/2000',
                                                      credit: 0, debit: 15.00,
                                                      balance: 20.00 }] )
-      expect { printer.print(log.transactions) }.to output(
+      expect { @printer.print(log.transactions) }.to output(
       'Date || Credit || Debit || Balance'\
       "\n01/01/2000 ||  || 15.00 || 20.00\n"
       ).to_stdout
   end
 
-  it 'prints statements in reverse chronological order' do
-      log = instance_double("log", :transactions => [{ date: '01/01/2017',
-                                                    credit: 0, debit: 15.00,
-                                                    balance: 20.00 },
-                                                    { date: '15/07/2000',
-                                                    credit: 0, debit: 15.00,
-                                                    balance: 20.00 },
-                                                    { date: '11/08/2005',
-                                                    credit: 0, debit: 15.00,
-                                                    balance: 20.00 }] )
-      expect { printer.print(log.transactions) }.to output(
-      'Date || Credit || Debit || Balance'\
-      "\n15/07/2000 ||  || 15.00 || 20.00\n"\
-      "\n11/08/2005 ||  || 15.00 || 20.00\n"\
-      "\n01/01/2017 ||  || 15.00 || 20.00\n"
-      ).to_stdout
+  # it 'prints statements in reverse chronological order' do
+  #     log = instance_double("log", :transactions => [{ date: '01/01/2017',
+  #                                                   credit: 0, debit: 15.00,
+  #                                                   balance: 20.00 },
+  #                                                   { date: '15/07/2000',
+  #                                                   credit: 0, debit: 15.00,
+  #                                                   balance: 20.00 },
+  #                                                   { date: '11/08/2005',
+  #                                                   credit: 0, debit: 15.00,
+  #                                                   balance: 20.00 }] )
+  #     expect { @printer.print(log.transactions) }.to output(
+  #     'Date || Credit || Debit || Balance'\
+  #     "\n15/07/2000 ||  || 15.00 || 20.00\n"\
+  #     "\n11/08/2005 ||  || 15.00 || 20.00\n"\
+  #     "\n01/01/2017 ||  || 15.00 || 20.00\n"
+  #     ).to_stdout
+  # end
+
+  it 'calls sorter to put transactions in reverse chronological order' do
+    expect
   end
 end
