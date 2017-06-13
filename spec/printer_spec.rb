74e4423c39545e782012a8358f772a66fce57d68
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-13 11:07:02 +0100

    Adds failing tests for statements printing newest transactions first

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -4,7 +4,7 @@ describe Printer do
   let(:printer) { described_class.new }
 
   it 'correctly prints a statement with deposits' do
-    log = instance_double("log", :transactions => [{ date: '01/01/2000',
+      log = instance_double("log", :transactions => [{ date: '01/01/2000',
                                                      credit: 10.00, debit: 0,
                                                      balance: 10.00 }] )
       expect { printer.print(log.transactions) }.to output(
@@ -14,12 +14,26 @@ describe Printer do
   end
 
   it 'correctly prints a statement with withdrawals' do
-    log = instance_double("log", :transactions => [{ date: '01/01/2000',
+      log = instance_double("log", :transactions => [{ date: '01/01/2000',
                                                      credit: 0, debit: 15.00,
                                                      balance: 20.00 }] )
       expect { printer.print(log.transactions) }.to output(
       'Date || Credit || Debit || Balance'\
       "\n01/01/2000 ||  || 15.00 || 20.00\n"
+      ).to_stdout
+  end
+
+  it 'prints statements in reverse chronological order' do
+      log = instance_double("log", :transactions => [{ date: '01/01/2017',
+                                                    credit: 0, debit: 15.00,
+                                                    balance: 20.00 },
+                                                    { date: '15/07/2000',
+                                                    credit: 0, debit: 15.00,
+                                                    balance: 20.00 }] )
+      expect { printer.print(log.transactions) }.to output(
+      'Date || Credit || Debit || Balance'\
+      "\n15/07/2000 ||  || 15.00 || 20.00\n"\
+      "\n01/01/2017 ||  || 15.00 || 20.00\n"
       ).to_stdout
   end
 end
