b77e05384955eaa52c4ca9ba5ccb32a245ec1bec
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-13 15:06:19 +0100

    changes transactions in printer into regular double

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -5,29 +5,29 @@ describe Printer do
     allow(sorter).to receive_messages(:new => sorter, :sort => [])
   end
 
-  let(:transactions) { [] }
+  let(:transactions) { double }
   let(:sorter) { double }
 
   it 'correctly prints a statement with deposits' do
-      allow(sorter).to receive(:sort).with(anything) { [{ date: '01/01/2000',
-                                                   credit: 10.00, debit: 0,
-                                                   balance: 10.00 }] }
-      @printer = Printer.new(sorter)
-      expect { @printer.print(transactions) }.to output(
-      'Date || Credit || Debit || Balance'\
-      "\n01/01/2000 || 10.00 ||  || 10.00\n"
-      ).to_stdout
+    allow(sorter).to receive(:sort).with(anything) { [{ date: '01/01/2000',
+                                                 credit: 10.00, debit: 0,
+                                                 balance: 10.00 }] }
+    @printer = Printer.new(sorter)
+    expect { @printer.print(transactions) }.to output(
+    'Date || Credit || Debit || Balance'\
+    "\n01/01/2000 || 10.00 ||  || 10.00\n"
+    ).to_stdout
   end
 
   it 'correctly prints a statement with withdrawals' do
-      allow(sorter).to receive(:sort).with(anything) { [{ date: '01/01/2000',
-                                                     credit: 0, debit: 15.00,
-                                                     balance: 20.00 }] }
-      @printer = Printer.new(sorter)
-      expect { @printer.print(transactions) }.to output(
-      'Date || Credit || Debit || Balance'\
-      "\n01/01/2000 ||  || 15.00 || 20.00\n"
-      ).to_stdout
+    allow(sorter).to receive(:sort).with(anything) { [{ date: '01/01/2000',
+                                                   credit: 0, debit: 15.00,
+                                                   balance: 20.00 }] }
+    @printer = Printer.new(sorter)
+    expect { @printer.print(transactions) }.to output(
+    'Date || Credit || Debit || Balance'\
+    "\n01/01/2000 ||  || 15.00 || 20.00\n"
+    ).to_stdout
   end
 
   it 'calls sorter to put transactions in reverse chronological order' do
